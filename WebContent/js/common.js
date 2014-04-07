c612c0398798d5fd678bfbad39fceb0d8c78dc04
박종영 <hermeswing@gmail.com>
2014-04-08 01:08:00 +0900

    Board_V1 재정리

---
+++ WebContent/js/common.js
@@ -0,0 +1,96 @@
+$.extend({
+	// alert 창 띄우기
+	dialog: function(title, msg, modal) {
+		if(modal == undefined) {
+			modal = false;
+		}
+		
+		var dialogId = 'ui-dialog-' + parseInt(Math.random() * 100000);
+		
+		$('body').append($('<div />', {
+			'id': dialogId
+		}));
+		
+		$('#' + dialogId).attr('title', title);
+		$('#' + dialogId).html($('<p />', {
+			'html': msg
+		}));
+		
+		$('#' + dialogId).dialog({
+			'autoOpen': true,
+			'modal': modal,
+			buttons: {
+				'확인': function() {
+					$(this).dialog('close');
+				}
+			},
+			'close': function() {
+				$(this).remove();
+			}
+		});
+	}
+});
+
+$.fn.error = function(msg, o) {
+	if(o == undefined) {
+		o = {
+			override : true,
+			fadeOut: true,
+			fadeTime: 5000
+		};
+	}
+
+	if(o.override == true) {
+		$('#error').remove();
+	}
+		
+	// 바로 아래에 에러정보 표시
+	var error = $('<div />', {
+		'id': 'error',
+		'css': {
+			'border': '1px solid #ff0000',
+			'margin': 5,
+			'padding': 5,
+			'text-align': 'left'
+		}
+	}).append($('<img />', {
+		'src': 'images/error.png',
+		'css': {
+			'margin-bottom': -3
+		}
+	})).append($('<span />', {
+		'text': msg
+	}));
+		
+	$(this).append(error);
+	
+	if(o.fadeOut == true) {
+		if(o.fadeTime == undefined) {
+			o.fadeTime = 5000;
+		}
+		error.fadeOut(o.fadeTime, function() {
+			$(this).remove();
+		});
+	}
+};
+
+//바이트 용량을 단위 포함된 용량으로 변환
+$.fn.parseByteUnit = function() {
+	var bytes = $(this).get()[0];
+	
+	var kb = 1024;
+	var mb = 1024 * 1000;
+	var gb = 1024 * 1000 * 1000;
+	
+	if(bytes <= 1) {
+		return bytes + " Byte";
+	} else if(bytes < kb) {
+		return bytes + " Bytes";
+	} else if (bytes < mb) {
+		return parseInt(bytes / kb) + " KB";
+	} else if (bytes < gb) {
+		return parseInt(bytes / mb) + " MB";
+	} else {
+		return parseInt(bytes / gb) + " GB";
+	}
+};
